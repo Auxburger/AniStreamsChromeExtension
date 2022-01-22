97db94d61531e60fdcdbe18db6ec427a6f94005c
Darius Augsburger <darius.augsburger@gmail.com>
2022-01-22 01:23:52 +0100

    added button to open window for manual usages

--- content.js
+++ content.js
@@ -1,8 +1,22 @@
-console.log("DOM LOADED");
-setTimeout(() => {
+
+function openWindow() {
   let acceptButton = document.getElementsByClassName("iubenda-cs-accept-btn iubenda-cs-btn-primary");
   let iframe = document.getElementById("embed");
   if (iframe != null) {
     window.open(iframe.children[0].src, '_blank').focus();
   }
-}, 1000);
+
+}
+
+setTimeout(() => openWindow(), 100);
+
+// Listen for messages from the popup.
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+  // First, validate the message's structure.
+  if (msg.action === 'open') {
+    // Collect the necessary data. 
+    // (For your specific requirements `document.querySelectorAll(...)`
+    //  should be equivalent to jquery's `$(...)`.)
+    openWindow();
+  }
+});
---
+++ popup.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('#open').on("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "open" });
+    });
+  });
+});
